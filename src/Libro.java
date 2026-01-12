2325580968dc82099bd445fc1fa281d87d9b75fb
luis <luigimtre@alu.edu.gva.es>
2026-01-12 10:29:16 +0100

    Atriubtos privados + getter y setter

--- src/Libro.java
+++ src/Libro.java
@@ -1,8 +1,43 @@
 public class Libro {
-    String titulo;
-    String autor;
-    int numPag;
-    int valoracion;
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    private String titulo;
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    private String autor;
+
+    public int getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
+    }
+
+    private int numPag;
+
+    public int getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(int valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    private int valoracion;
     public void mostrarinfo(){
         System.out.println(titulo);
         System.out.println(autor);
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         Libro libro1 = new Libro();
-        libro1.titulo = "Don Quijote de la Mancha";
-        libro1.autor = "Miguel de Cervantes";
-        libro1.numPag = 833;
-        libro1.valoracion = 5;
+        libro1.setTitulo("Don Quijote de la Mancha");
+        libro1.setAutor("Miguel de Cervantes");
+        libro1.setNumPag(833);
+        libro1.setValoracion(4);
 
         Libro libro2 = new Libro();
-        libro2.titulo = "Cien años de soledad";
-        libro2.autor = "Gabriel García Márquez";
-        libro2.numPag = 423;
-        libro2.valoracion = 4;
+        libro2.setTitulo("Cien años de soledad");
+        libro2.setAutor("Gabriel García Márquez");
+        libro2.setNumPag(423);
+        libro2.setValoracion(4);
 
         libro1.mostrarinfo();
         System.out.println("");
