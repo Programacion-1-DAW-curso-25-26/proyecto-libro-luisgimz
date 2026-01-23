abaaee558a143c37eb7f29c183e888d75b6fb610
luis <luigimtre@alu.edu.gva.es>
2026-01-23 11:31:30 +0100

    Con menu

--- src/Libro.java
+++ src/Libro.java
@@ -1,54 +1,17 @@
 import java.util.Objects;
 
 public class Libro {
-
-   public Libro(String titulo,String autor,int numPag,int valoracion,int isbn){
-       this.titulo = titulo;
-       this.autor = autor;
-       this.numPag = numPag;
-       this.valoracion = valoracion;
-       this.isbn = isbn;
-   }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
     private String titulo;
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
     private String autor;
+    private int numpaginas;
+    private int isbn;
 
-    public int getNumPag() {
-        return numPag;
+    public Libro(String titulo, String autor, int numpaginas, int isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numpaginas = numpaginas;
+        this.isbn = isbn;
     }
-
-    public void setNumPag(int numPag) {
-        this.numPag = numPag;
-    }
-
-    private int numPag;
-
-    public int getValoracion() {
-        return valoracion;
-    }
-
-    public void setValoracion(int valoracion) {
-        this.valoracion = valoracion;
-    }
-
-    private int valoracion;
 
     public int getIsbn() {
         return isbn;
@@ -58,27 +21,42 @@ public class Libro {
         this.isbn = isbn;
     }
 
-    private int isbn;
+    public String getTitulo() {
+        return titulo;
+    }
 
-    public void mostrarinfo(){
-        System.out.println(titulo);
-        System.out.println(autor);
-        System.out.println(numPag);
-        System.out.println(valoracion);
-        System.out.println(isbn);
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumpaginas() {
+        return numpaginas;
+    }
+
+    public void setNumpaginas(int numpaginas) {
+        this.numpaginas = numpaginas;
+    }
+
+    void mostarInfo(){
+        System.out.println("Titulo = " + titulo);
+        System.out.println("Autor = " + autor);
+        System.out.println("ISBN = " + isbn);
+        System.out.println("NumPaginas = " + numpaginas);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Libro libro = (Libro) o;
         return isbn == libro.isbn;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(isbn);
     }
 
     @Override
@@ -86,10 +64,12 @@ public class Libro {
         return "Libro{" +
                 "titulo='" + titulo + '\'' +
                 ", autor='" + autor + '\'' +
-                ", numPag=" + numPag +
-                ", valoracion=" + valoracion +
+                ", numpaginas=" + numpaginas +
                 ", isbn=" + isbn +
                 '}';
     }
-
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(isbn);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,106 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-
-        Libro libro1 = new Libro("Don Quijote de la Mancha","Miguel de Cervantes",833,4,1);
-        Libro libro2 = new Libro("Don Quijote de la Mancho","Miguelon de Cervantes",823,4,1);
-
+public class Main{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char opcion;
         ArrayList<Libro> biblioteca = new ArrayList<>();
-        biblioteca.add(libro1);
-
-        System.out.println(biblioteca.contains(libro2));
-        //true por isbn hay otro
-
-
-        HashSet<Libro> casera = new HashSet<>();
-        System.out.println(casera.add(libro1)); //true
-        System.out.println(casera.add(libro2)); //false son el mismo isbn en hascode
-
-
+        do{
+            imprimirMenu();
+            opcion = sc.nextLine().toLowerCase().charAt(0);
+            switch (opcion){
+                case 'a':
+                    anyadirLibro(biblioteca, sc);
+                    break;
+                case 'b':
+                    mostarLibros(biblioteca);
+                    break;
+                case 'c':
+                    buscarLibro(biblioteca,sc);
+                    break;
+                case 'd':
+                    eliminarLibro(biblioteca,sc);
+                    break;
+                case 'e':
+                    vaciarLista(biblioteca);
+                    break;
+                case 'f':
+                    break;
+                default:
+                    System.out.println("opcion incorrecta");
+                    break;
+            }
+        } while (opcion!= 'f');
     }
 
+    private static void vaciarLista(ArrayList<Libro> biblioteca) {
+        int tamanyo = biblioteca.size();
+        biblioteca.clear();
+        System.out.println("Borrados : "+ tamanyo +" libros");
+    }
 
+    private static void eliminarLibro(ArrayList<Libro> biblioteca, Scanner sc) {
+        System.out.println("Introduce ISBN");
+        int isbnB = sc.nextInt();
+        sc.nextLine();
+        Iterator<Libro> it = biblioteca.iterator();
+        while (it.hasNext()){
+            Libro l =it.next();
+            if (l.getIsbn()==isbnB){
+                it.remove();
+            }
+        }
+    }
+
+    private static void buscarLibro(ArrayList<Libro> biblioteca,Scanner sc) {
+        System.out.println("Introduce ISBN");
+        int isbnB = sc.nextInt();
+        sc.nextLine();
+        for (Libro l : biblioteca){
+            if (l.getIsbn()==isbnB){
+                System.out.println(l);
+            } else {
+                System.out.println("No encontrado");
+            }
+        }
+    }
+
+    private static void mostarLibros(ArrayList<Libro> biblioteca) {
+        for (int i =0; i < biblioteca.size(); i++){
+            System.out.println(biblioteca.get(i));
+        }
+    }
+
+    public static void imprimirMenu() {
+        System.out.println("--- MENÚ LIBRERÍA ---\n" +
+                "a. Añadir Libro\n" +
+                "b. Mostrar Libros\n" +
+                "c. Buscar por ISBN\n" +
+                "d. Eliminar por ISBN\n" +
+                "e. Vaciar lista\n" +
+                "f. Salir\n" +
+                "Seleccione opción:");
+    }
+
+    private static void anyadirLibro(ArrayList<Libro> biblioteca, Scanner sc) {
+        String nombre;
+        String autor;
+        int numPaginas;
+        int isbn;
+        System.out.println("Dime el nombre");
+        nombre = sc.nextLine();
+        System.out.println("Dime el autor");
+        autor = sc.nextLine();
+        System.out.println("Dime el número de páginas");
+        numPaginas = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Dime el ISBN");
+        isbn = sc.nextInt();
+        sc.nextLine();
+        Libro nuevo = new Libro(nombre, autor, numPaginas,isbn);
+        biblioteca.add(nuevo);
+    }
 }
